94203150ad74b42e64edd59d05447bb33298fe80
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2020-06-28 18:56:21 +0300

    8_get_request_from_frontend_to_backend

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getAllStudents } from './client';
 
 class App extends Component {
   render() {
+    getAllStudents().then(res => res.json().then(students => {
+      console.log(students);
+    }));
     return <h1>Hello World Spring Boot & React</h1>
   }
 }
