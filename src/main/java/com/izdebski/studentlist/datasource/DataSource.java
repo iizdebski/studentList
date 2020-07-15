261ff224ef44819fa6dc2a24543f5bff8bf63ec7
iizdebski <39988554+iizdebski@users.noreply.github.com>
2020-07-15 18:27:07 +0300

    sc5_30_adding_students_to_database

--- src/main/java/com/izdebski/studentlist/datasource/DataSource.java
+++ src/main/java/com/izdebski/studentlist/datasource/DataSource.java
@@ -4,9 +4,9 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class DataSource {
 
     @Bean
--- src/main/java/com/izdebski/studentlist/student/StudentDataAccessService.java
+++ src/main/java/com/izdebski/studentlist/student/StudentDataAccessService.java
@@ -1,5 +1,9 @@
 package com.izdebski.studentlist.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,23 +12,42 @@ import java.util.UUID;
 @Repository
 public class StudentDataAccessService {
 
-    public List<Student> selectAllStudents() {
+    // Class-JdbcTemplate allows us to interacted with our Database
+    private final JdbcTemplate jdbcTemplate;
 
-        return List.of(
-                new Student(
-                        UUID.randomUUID(),
-                        "James",
-                        "Bond",
-                        "jamesbond@gmail.com",
-                        Student.Gender.MALE
-                ),
-                new Student(
-                        UUID.randomUUID(),
-                        "Elisa",
-                        "Tamara",
-                        "elisaTamara@hotmail.com",
-                        Student.Gender.FEMALE
-                )
-        );
+    @Autowired
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    List<Student> selectAllStudents() {
+        String sql = "" +
+                "SELECT" +
+                " student_id," +
+                " first_name," +
+                " last_name," +
+                " email," +
+                " gender " +
+                "FROM student;";
+
+        // sql statement : e.g. 'SELECT uuid, name FROM students;'
+        return jdbcTemplate.query(sql, mapStudentFromDb());
+    }
+
+
+    private RowMapper<Student> mapStudentFromDb() {
+        return (resultSet, i) -> {   // returns raw Db as 'Set'. Each row from Db Set is mapped to own index 'i'
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+
+            String genderStr = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+
+            return new Student(studentId, firstName, lastName, email, gender);
+        };
     }
 }
